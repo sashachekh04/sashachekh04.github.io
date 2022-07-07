7ab16eb8520f86d8187feb1bfe08a6791f902866
Sasha Chekh <cheh170804@gmail.com>
2022-07-07 20:02:19 +0300

    feat(loan-list): add implementation from data

--- src/app/components/loan-list/loan-list.component.ts
+++ src/app/components/loan-list/loan-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoanService } from "../../core/services/loan.service";
+import { Observable } from "rxjs";
+import { Loan } from "../../core/model/loan";
 
 @Component({
   selector: 'app-loan-list',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loan-list.component.scss']
 })
 export class LoanListComponent implements OnInit {
+  public loanList: Observable<Loan[]>;
+  public availabel!: number;
 
-  constructor() { }
+  constructor(private loanService: LoanService) {
+    this.loanList = this.loanService.data
+    this.availabel = this.loanService.availableForInvesting
+  }
 
   ngOnInit(): void {
+    this.loanList.subscribe(el => console.log(el))
   }
 
 }
