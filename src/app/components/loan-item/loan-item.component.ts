2a58c6690f4b74d79026e50bfff2fbe1962512b0
Sasha Chekh <cheh170804@gmail.com>
2022-07-07 15:51:20 +0300

    feat(modal): add layout for Modal Component

--- src/app/components/loan-item/loan-item.component.ts
+++ src/app/components/loan-item/loan-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDialogComponent } from "../modal-dialog/modal-dialog.component";
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-loan-item',
@@ -7,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {
+  }
 
+  public openModal(): void {
+    this.dialog.open(ModalDialogComponent)
+  }
   ngOnInit(): void {
   }
 
