f5b3dd4a0e37f07d2fef1c1523ac88671dd12ae1
Sasha Chekh <cheh170804@gmail.com>
2022-07-07 20:01:31 +0300

    refactor(loan-data): change type of available

--- src/app/core/model/loan.ts
+++ src/app/core/model/loan.ts
@@ -2,7 +2,7 @@ export interface Loan {
   id: string | number,
   title: string,
   tranche: string,
-  available: string,
+  available: number,
   annualised_return: string,
   term_remaining: string,
   ltv: string,
