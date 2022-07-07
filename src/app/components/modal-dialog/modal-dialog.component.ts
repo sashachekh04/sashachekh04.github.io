80d95e7d388d5627e03175dbcd628a7bf9550829
Sasha Chekh <cheh170804@gmail.com>
2022-07-07 20:03:53 +0300

    feat(dialog window): add form logic

--- src/app/components/modal-dialog/modal-dialog.component.ts
+++ src/app/components/modal-dialog/modal-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Loan } from "../../core/model/loan";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ValidatePhone } from "../../core/validators/only-numbers";
+// import { onlyNumber } from "../../core/validators/only-numbers";
 
 @Component({
   selector: 'app-modal-dialog',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-dialog.component.scss']
 })
 export class ModalDialogComponent implements OnInit {
+  public timeToEnd!: any;
 
-  constructor() { }
+  public investForm: FormGroup = this.fb.group({
+    amount: ['', [Validators.required, ValidatePhone]]
+  })
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Loan,
+              private fb: FormBuilder) {
+    this.timeToEnd = this.endtime
+  }
 
   ngOnInit(): void {
   }
-
+  public get endtime(): Object {
+    const day = Math.floor(+this.data.term_remaining / (100*60*60*24)),
+          hours = Math.floor(+this.data.term_remaining / (100*60*60) % 24),
+          minutes = Math.floor((+this.data.term_remaining/1000/60) % 60)
+    return {
+      'day': day,
+      'hours': hours,
+      'minutes': minutes
+    }
+  }
+  public onSubmitForm(): void {
+    if(this.investForm.invalid) {
+      return;
+    }
+    console.log(this.investForm)
+  }
 }
