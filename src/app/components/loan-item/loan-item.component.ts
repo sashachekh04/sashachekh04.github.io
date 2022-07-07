d473adc7b3c85ed66a5a4047c774cabe8b8bf4aa
Sasha Chekh <cheh170804@gmail.com>
2022-07-07 20:02:47 +0300

    feat(loan-item): add implementation from input data

--- src/app/components/loan-item/loan-item.component.ts
+++ src/app/components/loan-item/loan-item.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalDialogComponent } from "../modal-dialog/modal-dialog.component";
 import {MatDialog} from '@angular/material/dialog';
+import { Loan } from "../../core/model/loan";
+import { LoanService } from "../../core/services/loan.service";
 
 @Component({
   selector: 'app-loan-item',
@@ -8,14 +10,21 @@ import {MatDialog} from '@angular/material/dialog';
   styleUrls: ['./loan-item.component.scss']
 })
 export class LoanItemComponent implements OnInit {
+  @Input() loanItem!: Loan;
 
   constructor(public dialog: MatDialog) {
   }
 
-  public openModal(): void {
-    this.dialog.open(ModalDialogComponent)
+  public openModal(loan: Loan): void {
+    this.dialog.open(ModalDialogComponent, {data: loan})
   }
   ngOnInit(): void {
   }
 
+  public get title(): string {
+    return this.loanItem.title
+  }
+  public get tranche(): string {
+    return this.loanItem.tranche
+  }
 }
